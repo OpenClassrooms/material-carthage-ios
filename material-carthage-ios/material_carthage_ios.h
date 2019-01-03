03157179fe636ed129a3c09e6efb2853db9f31ae
WANG Jie <jacques09125715@gmail.com>
2019-01-03 11:18:46 +0100

    Initial Commit

---
+++ material-carthage-ios/material_carthage_ios.h
@@ -0,0 +1,16 @@
+//
+//  Created by Jie Wang on 1/3/19.
+//  Copyright © 2019 OpenClassrooms. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for material_carthage_ios.
+FOUNDATION_EXPORT double material_carthage_iosVersionNumber;
+
+//! Project version string for material_carthage_ios.
+FOUNDATION_EXPORT const unsigned char material_carthage_iosVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <material_carthage_ios/PublicHeader.h>
+
+
