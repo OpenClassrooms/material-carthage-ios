79bd51a6c389c247bb4cdca3839b5a65d7f1aab2
cyr-lepottier <cyr.lepottier@gmail.com>
2023-11-02 10:30:50 +0100

    Update Xcode: 13.4.1 → 15.0.1
    Apply Xcode 15 recommended settings except for `ENABLE_USER_SCRIPT_SANDBOXING`. It has been manually set to `false` because it prevents a CocoaPods script from executing.

--- Tools/Test/run-unit-tests.sh
+++ Tools/Test/run-unit-tests.sh
@@ -5,4 +5,4 @@ DERIVED_DATA_DIR="$PWD/XcodeDerivedData"
 xcodebuild test \
 	-workspace material-carthage-ios.xcworkspace \
 	-scheme material-carthage-ios \
-	-destination 'platform=iOS Simulator,name=iPhone 13,OS=15.5' 2>&1 | xcbeautify && exit ${PIPESTATUS[0]}
+	-destination 'platform=iOS Simulator,name=iPhone 15,OS=17.0.1' 2>&1 | xcbeautify && exit ${PIPESTATUS[0]}
