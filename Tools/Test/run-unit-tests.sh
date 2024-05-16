75562295cfbc2673e63256d229731666eb8d8a2a
cyr-lepottier <cyr.lepottier@gmail.com>
2024-05-16 13:20:28 +0200

    Update Xcode: 15.2 → 15.4

--- Tools/Test/run-unit-tests.sh
+++ Tools/Test/run-unit-tests.sh
@@ -5,4 +5,4 @@ DERIVED_DATA_DIR="$PWD/XcodeDerivedData"
 xcodebuild test \
 	-workspace material-carthage-ios.xcworkspace \
 	-scheme material-carthage-ios \
-	-destination 'platform=iOS Simulator,name=iPhone 15,OS=17.2' 2>&1 | xcbeautify && exit ${PIPESTATUS[0]}
+	-destination 'platform=iOS Simulator,name=iPhone 15,OS=17.5' 2>&1 | xcbeautify && exit ${PIPESTATUS[0]}
