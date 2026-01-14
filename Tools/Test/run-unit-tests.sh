0bbf9bc0b6aaad5a2f13b7797f4b0474fe74de9d
Cyril Le Pottier <5039489+cyril-lepottier@users.noreply.github.com>
2026-01-14 14:39:02 +0100

    Update Xcode: 16.3 → 26.2
    It also applies the Xcode 26.2 recommended settings `STRING_CATALOG_GENERATE_SYMBOLS`.
    
    Note: Apple skipped Xcode versions 17 to 25 as a consequence of its operating systems naming conventions update. Consequently, all operating systems (iOS, iPadOS, macOS, etc.) and related products, such as Xcode, now have a unified version based on the current calendar year.

--- Tools/Test/run-unit-tests.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-DERIVED_DATA_DIR="$PWD/XcodeDerivedData"
-
-xcodebuild test \
-	-workspace material-carthage-ios.xcworkspace \
-	-scheme material-carthage-ios \
-	-destination 'platform=iOS Simulator,name=iPhone 16,OS=18.4' 2>&1 | xcbeautify && exit ${PIPESTATUS[0]}
